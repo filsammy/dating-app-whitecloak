d64e871c9031658a44a174f578af62bcd2037099
filsammy <filsammy@gmail.com>
2025-10-30 20:07:49 +0800

    revert api URL

--- client/api/messages.ts
+++ client/api/messages.ts
@@ -1,6 +1,6 @@
 import type { Match, Message } from "@/app/(protected)/chat/page"; // adjust path as needed
 
-const BASE_URL = process.env.API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 function getAuthHeader() {
   const token = localStorage.getItem("accessToken");
